a5189331ec34a52ebfd474f9bc907f376401ce1a
elaizae <43956037+elaizae@users.noreply.github.com>
2020-03-26 14:09:33 +0100

    version4

--- opdracht3/js/script.js
+++ opdracht3/js/script.js
@@ -3,47 +3,71 @@
 /*eslint-env browser*/
 
 /*eslint 'no-console': 0*/
-document.querySelector("body > section:nth-child(2) > div > form > div.search.my-3 > button");
+var section = document.querySelector('.middle');
 
-let requestURL = 'http://makeup-api.herokuapp.com/api/v1/products.json';
 
-let request = new
-XMLHttpRequest();
+var requestURL = 'https://raw.githubusercontent.com/KoopReynders/frontendvoordesigners/master/opdracht3/json/movies.json';
+
+var request = new XMLHttpRequest();
+
 request.open('GET', requestURL);
+
 request.responseType = 'json';
 request.send();
+console.log(request);
 
 request.onload = function () {
-    const makeUp =
-        request.response;
-    showProducts(makeUp);
-}
-
-
-function showProducts(jsonObj) {
-    const everything = jsonObj['products'];
-
-
-    for (let i = 0; i < everything.length; i++) {
-
-        const myArticle = document.createElement('article');
-
-        const myImg = document.createElement('img');
-        const myH2 = document.createElement('h2');
-        const discription = document.createElement('p');
+    var jsonObj = request.response;
+    showMovie(jsonObj);
 
 
 
 
-        myH2.textContent = everything[i].name;
-        discription.textContent = 'Discription: ' + everything[i].discription;
-        myImg.textContent = everything[i].image_link;
+};
+
+function showMovie(jsonObj) {
+    var info = jsonObj;
+    for (var i = 0; i < info.length; i++) {
+        var myArticle = document.createElement('article');
+        var infoButton = document.createElement('button');
+        var myH2 = document.createElement('h2');
+        var myPara1 = document.createElement('p');
+        var myPara2 = document.createElement('p');
+        var myPara3 = document.createElement('p');
+        var myPara4 = document.createElement('p');
 
 
+        myH2.textContent = info[i].title;
+        myPara1.textContent = info[i].genres;
+        myPara2.textContent = 'Release: ' + info[i].release_date;
+        myPara3.textContent = info[i].plot;
+        // myPara4.textContent = info[i].plot;
+
+
+
+        myArticle.appendChild(infoButton);
         myArticle.appendChild(myH2);
-        myArticle.appendChild(discription);
-        myArticle.appendChild(myImg);
+        myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        myArticle.appendChild(myPara3);
+        myArticle.appendChild(myPara4);
+
+
+
 
         section.appendChild(myArticle);
+
+        infoButton.onclick = function () {
+            console.log(this);
+            this.parentElement.classList.toggle('active');
+
+
+
+
+        };
+
     }
+
+
+
 }
