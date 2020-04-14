13bf17b9a5f845b6ad3f5b4c266ca785bc7a9b60
elaizae <43956037+elaizae@users.noreply.github.com>
2020-04-14 12:05:23 +0200

    version 5 or 6

--- opdracht3/js/script.js
+++ opdracht3/js/script.js
@@ -3,17 +3,23 @@
 /*eslint-env browser*/
 
 /*eslint 'no-console': 0*/
+
 var section = document.querySelector('.middle');
 
+//code of movies te laten zien na het klikken van het knopje "all"
 var laatzien = document.querySelector(".showme");
-laatzien.addEventListener("click", function () {
+laatzien.addEventListener("click", function showMovie() {
     section.innerHTML = "";
     loadContent();
 });
 
+/*var laatCrimezien = document.querySelector(".crime");
+laatCrimezien.addEventListener("click", function showCrime() {
+    section.innerHTML = "";
+    loadContent();
+});
 
-
-
+*/
 
 
 var requestURL = 'https://raw.githubusercontent.com/KoopReynders/frontendvoordesigners/master/opdracht3/json/movies.json';
@@ -31,6 +37,7 @@ function loadContent() {
         var jsonObj = request.response;
         showMovie(jsonObj);
 
+
     };
 }
 
@@ -46,6 +53,14 @@ function showMovie(jsonObj) {
         var myPara3 = document.createElement('p');
         var myPara4 = document.createElement('p');
 
+        // code below from stackoverflow modified to fit my code
+        //    https://stackoverflow.com/questions/42143031/filter-json-by-key-value
+
+
+
+
+
+        //end of stackoverflow code
 
         myH2.textContent = info[i].title;
         myCover.src = info[i].cover;
@@ -56,9 +71,10 @@ function showMovie(jsonObj) {
 
 
 
-        myArticle.appendChild(infoButton);
 
+        myArticle.appendChild(infoButton);
         myArticle.appendChild(myCover);
+
         myArticle.appendChild(myH2);
         myArticle.appendChild(myPara1);
         myArticle.appendChild(myPara2);
@@ -79,13 +95,23 @@ function showMovie(jsonObj) {
                 this.parentElement.classList.toggle('showittoo');
 
 
+
             };
 
         };
 
+
+
+
     }
+
+
 }
 
+
+
+
+// code of theme van de pagina te veranderen
 var icon = document.querySelector(".theme");
 icon.addEventListener("click", function () {
     icon.classList.toggle("night");
@@ -97,18 +123,43 @@ change.addEventListener("click", function () {
     theme.classList.toggle("day");
 });
 
-
-//2 clicks on space bar will change the theme  
+// clicks on arrowUp and arrowDown will change the theme 
 document.onkeydown = function (e) {
-    if (e.keyCode == 32) {
-        var icon = document.querySelector(".theme");
-        icon.addEventListener("click", function () {
-            icon.classList.toggle("night");
-        });
-        change.addEventListener("click", function () {
-            theme.classList.toggle("day");
-            console.log('Space pressed');
+    if (e.keyCode == 38) {
+        icon.click();
 
-        });
     }
+    if (e.keyCode == 40) {
+        change.click();
+    }
+
+    if (e.keyCode == 50)
+        first.click();
+
+    if (e.keyCode == 51)
+        first.click();
 };
+
+
+
+var first = document.querySelector(".col");
+var element = document.querySelector(".middle");
+first.addEventListener("click", function () {
+    element.classList.toggle("changeme");
+
+});
+
+var three = document.querySelector(".col");
+three.addEventListener("click", function () {
+    three.classList.toggle("two");
+});
+
+/* document.onreadystatechange = function() { 
+    if (document.readyState !== "complete") { 
+        document.querySelector(".middle").style.visibility = "hidden"; 
+        document.querySelector("#loader").style.visibility = "visible"; 
+    } else { 
+        document.querySelector("#loader").style.display = "none"; 
+        document.querySelector(".middle").style.visibility = "visible"; 
+    } 
+}; */
